4f6aee39bf9abdf2afec3c511073133748b7e816
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-12-04 09:27:51 -0500

    redis

--- examples/main.tf
+++ examples/main.tf
@@ -1,24 +1,3 @@
-# module "vpc" {
-#   source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.8.1"
-#   namespace  = var.namespace
-#   stage      = var.stage
-#   name       = var.name
-#   cidr_block = "172.16.0.0/16"
-# }
-
-# module "subnets" {
-#   source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.18.1"
-#   availability_zones   = var.availability_zones
-#   namespace            = var.namespace
-#   stage                = var.stage
-#   name                 = var.name
-#   vpc_id               = module.vpc.vpc_id
-#   igw_id               = module.vpc.igw_id
-#   cidr_block           = module.vpc.vpc_cidr_block
-#   nat_gateway_enabled  = true
-#   nat_instance_enabled = false
-# }
-
 module "redis" {
   source                     = "git::https://github.com/tonygyerr/terraform-aws-elasticache-redis.git"
   availability_zones         = var.availability_zones
@@ -27,17 +6,17 @@ module "redis" {
   stage                      = var.stage
   name                       = var.name
   zone_id                    = var.zone_id
-  vpc_id                     = var.vpc_config.vpc_id               #module.vpc.vpc_id
+  vpc_id                     = var.vpc_id
   allowed_security_groups    = [module.redis.security_group_id] #[var.vpc_config.security_group_ids]   #[module.vpc.api_security_group_id]
-  subnet_ids                 = var.subnet_ids #[var.vpc_config.private_db_subnets] #module.vpc.private_db_subnets #module.vpc.private_db_subnets
+  subnet_ids                 = var.subnet_ids #[var.vpc_config.private_db_subnets] #module.vpc.private_db_subnets
   cluster_size               = var.cluster_size
   instance_type              = var.instance_type
   number_of_subnets          = var.number_of_subnets
-  private_db_subnets         = var.private_db_subnets
   apply_immediately          = true
   automatic_failover_enabled = var.automatic_failover_enabled
   engine_version             = var.engine_version
   family                     = var.family
+  replication_group_description = var.replication_group_description
   at_rest_encryption_enabled = var.at_rest_encryption_enabled
   transit_encryption_enabled = var.transit_encryption_enabled
 
